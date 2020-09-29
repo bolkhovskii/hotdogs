dff078aeb07f9399a9d8f63d2254ced95c228fab
Bolkhovskyi Oleksii <bolkhovskiy.a@gmail.com>
2020-09-29 17:16:40 +0300

    Added deleteuserbyid

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,19 +1,18 @@
-
 const convert = require("koa-convert");
 const KoaBody = require("koa-body");
 const Router = require("koa-router");
 
-const userController = require('../controllers/user');
+const userController = require("../controllers/user");
 const routers = new Router();
 
 //Authorization
 
 routers
-  .get('/users', userController.getAllUsers)
-  .post('/register', KoaBody(),  userController.addNewUser)
+  .get("/users", userController.getAllUsers)
+  .post("/register", KoaBody(), userController.addNewUser)
+  .delete("/user/:id", convert(KoaBody()), userController.deleteUserById);
 
 module.exports = routers;
-
 
 // const paramValidation = require('../config/paramValidation');
 // const customerController = require('../controllers/customer');
