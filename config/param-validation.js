6b93415afe0cc01e747863f86c0535fa2d368a43
Bolkhovskyi Oleksii <bolkhovskiy.a@gmail.com>
2020-10-01 16:45:07 +0300

    Auth validation

--- config/param-validation.js
+++ config/param-validation.js
@@ -1,15 +1,23 @@
-const Joi = require("joi");
+const { Joi } = require("koa-joi-router");
 const validate = require("koa-joi-validate");
 
 const loginValidator = validate({
-  username: Joi.string().required(),
-  password: Joi.string().required(),
+  body: {
+    email: Joi.string().email().min(6),
+    password: Joi.string().min(6).max(32).required(),
+  },
 });
 
 const registerValidator = validate({
-  username: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  body: {
+    username: Joi.string().min(3).max(16).required(),
+    email: Joi.string().email().min(6).required(),
+    password: Joi.string().min(6).max(32).required(),
+  },
 });
-
-module.exports = { loginValidator, registerValidator };
+// const registerValidator = validate({
+//   username: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   password: Joi.string().required(),
+// });
+module.exports = { loginValidator };
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,21 +1,38 @@
 const convert = require("koa-convert");
 const KoaBody = require("koa-body");
-const Router = require("koa-router");
-const passport = require('koa-passport')
+//const Router = require("koa-router");
+const Router = require("koa-joi-router");
+const passport = require("koa-passport");
+const validate = require("koa-joi-validate");
 
 const authController = require("../controllers/auth");
-
 const userController = require("../controllers/user");
-const routers = new Router().prefix('/api');
+const routers = new Router().prefix("/api");
+
+const { loginValidator } = require("../config/param-validation");
+const { Joi } = require("koa-joi-router");
 
 routers
   //users
-  .get("/users", passport.authenticate('jwt', { session: false }), userController.getAllUsers)
+  .get(
+    "/users",
+    passport.authenticate("jwt", { session: false }),
+    userController.getAllUsers
+  )
   .post("/register", KoaBody(), userController.addNewUser)
   .delete("/user/:id", convert(KoaBody()), userController.deleteUserById)
 
   //Authorization
-  .post("/auth/signup", authController.signUp) 
-  .post("/auth/signin", authController.signIn);
+  .post("/auth/signup", authController.signUp)
+  // .post(
+  //   "/auth/signin",
+  //   validate({
+  //     body: {
+  //       email: Joi.string().email().min(6),
+  //     },
+  //   }),
+  //   authController.signIn
+  // );
+  .post("/auth/signin", loginValidator, authController.signIn);
 
 module.exports = routers;
